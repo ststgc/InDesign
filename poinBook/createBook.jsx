eef2d324485a8fcc419e74319d4e30f4a1c5ec70
ststgc <ststgc@gmail.com>
2015-07-30 16:24:08 +0900

    first commit

---
+++ poinBook/createBook.jsx
@@ -0,0 +1,20 @@
+
+
+
+// Main
+#include "./loadBookData.jsxinc"
+#include "./documentSettings.jsxinc"
+#include "./config.jsxinc"
+#include "./saveBookAssets.jsxinc"
+//#include "./createBookCoverFront.jsxinc"
+#include "./createPageFront.jsxinc"
+#include "./createPages.jsxinc"
+#include "./createStatsPage.jsxinc"
+//#include "./createBookCoverBack.jsxinc"
+#include "./exportAsPDF.jsxinc"
+
+
+
+
+
+
