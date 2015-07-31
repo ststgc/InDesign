f3ba223fb4b6e7dbae448a79126f39962785a7d3
ststgc <ststgc@gmail.com>
2015-07-31 20:04:46 +0900

    fix visibleBounds position

--- poinBook/createBook.jsx
+++ poinBook/createBook.jsx
@@ -6,10 +6,10 @@
 #include "./documentSettings.jsxinc"
 #include "./config.jsxinc"
 #include "./saveBookAssets.jsxinc"
-//#include "./createBookCoverFront.jsxinc"
 #include "./createPageFront.jsxinc"
 #include "./createPages.jsxinc"
 #include "./createStatsPage.jsxinc"
+//#include "./createBookCoverFront.jsxinc"
 //#include "./createBookCoverBack.jsxinc"
 #include "./exportAsPDF.jsxinc"
 
