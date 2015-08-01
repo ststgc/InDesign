897b8a4cc569fa74eab6ede004a3348854bf34bd
ststgc <ststgc@gmail.com>
2015-08-01 13:47:16 +0900

    add flags

--- poinBook/createBook.jsx
+++ poinBook/createBook.jsx
@@ -2,15 +2,18 @@
 
 
 // Main
+
+
 #include "./loadBookData.jsxinc"
 #include "./documentSettings.jsxinc"
 #include "./config.jsxinc"
+#include "./flags.jsxinc"
 #include "./saveBookAssets.jsxinc"
 #include "./createPageFront.jsxinc"
 #include "./createPages.jsxinc"
 #include "./createStatsPage.jsxinc"
-//#include "./createBookCoverFront.jsxinc"
-//#include "./createBookCoverBack.jsxinc"
+// #include "./createBookCoverFront.jsxinc"
+// #include "./createBookCoverBack.jsxinc"
 #include "./exportAsPDF.jsxinc"
 
 
