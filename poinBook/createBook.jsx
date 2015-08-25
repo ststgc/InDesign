a84f55c225f2b4f80c20a624305e610244745c62
ststgc <ststgc@gmail.com>
2015-08-25 11:57:37 +0900

    change from text to image

--- poinBook/createBook.jsx
+++ poinBook/createBook.jsx
@@ -6,12 +6,12 @@
 #include "./documentSettings.jsxinc"
 #include "./config.jsxinc"
 #include "./flags.jsxinc"
-#include "./saveBookAssets.jsxinc"
-#include "./createPageFront.jsxinc"
-#include "./createPages.jsxinc"
-#include "./createStatsPage.jsxinc"
+// #include "./saveBookAssets.jsxinc"
+// #include "./createPageFront.jsxinc"
+// #include "./createPages.jsxinc"
+// #include "./createStatsPage.jsxinc"
 #include "./createBookCover.jsxinc"
-#include "./exportAsPDF.jsxinc"
+// #include "./exportAsPDF.jsxinc"
 
 
 
