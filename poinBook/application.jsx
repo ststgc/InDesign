ddc0368ad3d802462f19f0fa616c08d070c705de
Shota Setoguchi <shota@Shota-no-MacBook-Pro.local>
2015-10-14 13:47:40 +0900

    .

---
+++ poinBook/application.jsx
@@ -0,0 +1,15 @@
+
+// Main
+#include "./loadBookData.jsxinc"
+#include "./documentSettings.jsxinc"
+#include "./config.jsxinc"
+#include "./flags.jsxinc"
+#include "./saveBookAssets.jsxinc"
+#include "./createPageFront.jsxinc"
+#include "./createPages.jsxinc"
+#include "./createStatsPage.jsxinc"
+#include "./createBookCover.jsxinc"
+#include "./exportAsPDF.jsxinc"
+
+
+
